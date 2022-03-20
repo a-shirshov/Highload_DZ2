8eb08af60b6e56db4afcb42091d1743a557954b9
Artyom Shirshov <artyomsh01@yandex.ru>
2022-03-20 17:06:21 +0300

    Working Version

---
+++ build/CMakeFiles/response.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/response.dir/lib/server/response.cpp.o"
+  "libresponse.a"
+  "libresponse.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/response.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
